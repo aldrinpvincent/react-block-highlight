6dbebd25c3219f4a82880c1e16509329763d68c9
aldrin v <aldrin.v@SW-LP08548.local>
2019-07-30 15:25:55 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useRef } from "react";
+import PropTypes from 'prop-types';
+/**
+ * React Component to highlight any part of the containing block in reactangle size.
+ */
+
+const ReactBlockHighlighter = ({ id, x, y, height, width, opacity, color }) => {
+  const canvasRef = useRef(null);
+  const ctx = useRef();
+  const canvas = useRef({});
+
+  useEffect(() => {
+    ctx.current = canvasRef.current.getContext("2d");
+  }, []);
+
+  useEffect(() => {
+    canvas.current = document.getElementById(id) || {};
+    ctx.current.clearRect(0, 0, canvas.current.width, canvas.current.height);
+    ctx.current.globalAlpha = opacity;
+    ctx.current.fillStyle = color;
+    ctx.current.fillRect(x, y, width, height);
+    ctx.current.lineWidth = .2;
+    ctx.current.strokeStyle = "#FF0000";
+    ctx.current.strokeRect(x, y, width, height);
+  }, [x, y, height, width, id, color, opacity]);
+
+  return (
+    <section style={{ width: "100%", height: "100%" }}>
+      <canvas style={{ width: "100%", height: "100%" }} ref={canvasRef} id={id} />
+    </section>
+  );
+};
+
+ReactBlockHighlighter.propTypes = {
+  /** Unique id for the canvas element */
+  id: PropTypes.string,
+  /** x-coordinate of origin */
+  x: PropTypes.number,
+  /** y-coordinate of origin */
+  y: PropTypes.number,
+  /** Height of the rectangle to be highlighted */
+  height: PropTypes.number,
+  /** Width of the rectangle to be highlighted */
+  width: PropTypes.number,
+  /** Opacity of the rectangle to be highlighted */
+  opacity: PropTypes.number,
+  /** Color of the rectangle to be highlighted */
+  color: PropTypes.string
+}
+
+ReactBlockHighlighter.defaultProps = {
+  id: "block-highlight-canvas",
+  opacity: .5,
+  color: "#ffff00",
+  x: 0,
+  y: 0
+};
+
+export default ReactBlockHighlighter;
